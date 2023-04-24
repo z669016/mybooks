d18d3ed1bbb0157dc459d2aecd4dc18f40bb4289
Rene van Putten <z669016@gmail.com>
2023-04-24 21:57:30 +0200

    Completed BookControllerTest for postBook method

--- src/test/java/com/putoet/mybooks/books/adapter/in/web/BookControllerTest.java
+++ src/test/java/com/putoet/mybooks/books/adapter/in/web/BookControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -22,7 +21,8 @@ class BookControllerTest {
     private BookUpdateService bookUpdateService;
     private BookController bookController;
 
-    private final Author author = new Author(AuthorId.withoutId(), Instant.now(), "Schrijver, Jaap de", Map.of());
+    private final Author author = new Author(AuthorId.withoutId(), "Schrijver, Jaap de");
+
     private final List<String> formats = List.of(MimeTypes.PDF.toString(), MimeTypes.EPUB.toString());
     private final Book book = new Book(new BookId(BookId.BookIdScheme.ISBN, "978-1-83921-196-6"),
             "Get Your Hands Dirty on Clean Architecture",
@@ -30,6 +30,7 @@ class BookControllerTest {
             Set.of("architecture", "rest"),
             new MimeTypes(BookResponse.toDomain(formats))
     );
+
     @BeforeEach
     void setup() {
         bookInquiryService = mock(BookInquiryService.class);
@@ -58,7 +59,7 @@ class BookControllerTest {
 
     @Test
     void getBooksByAuthorName() {
-        final List<BookResponse> books = bookController.getBooksByAuthorName(author.name());
+        bookController.getBooksByAuthorName(author.name());
         verify(bookInquiryService, times(1)).booksByAuthorName(author.name());
     }
 
@@ -76,7 +77,7 @@ class BookControllerTest {
 
     @Test
     void getBooksByTitle() {
-        final List<BookResponse> books = bookController.getBooksByTitle(book.title());
+        bookController.getBooksByTitle(book.title());
         verify(bookInquiryService, times(1)).booksByTitle(book.title());
     }
 
@@ -91,12 +92,14 @@ class BookControllerTest {
             assertEquals(HttpStatus.BAD_REQUEST, exc.getStatusCode());
         }
     }
+
     @Test
     void getBookById() {
         when(bookInquiryService.bookById(book.id())).thenReturn(Optional.of(book));
         bookController.getBookById(book.id().schema().name(), book.id().id());
         verify(bookInquiryService, times(1)).bookById(book.id());
     }
+
     @Test
     void getBookByIdFailed() {
         try {
@@ -105,6 +108,7 @@ class BookControllerTest {
             assertEquals(HttpStatus.BAD_REQUEST, exc.getStatusCode());
         }
     }
+
     @Test
     void getBookByIdNotFound() {
         when(bookInquiryService.bookById(book.id())).thenReturn(Optional.empty());
@@ -114,7 +118,49 @@ class BookControllerTest {
             assertEquals(HttpStatus.NOT_FOUND, exc.getStatusCode());
         }
     }
+
     @Test
     void postBook() {
+        final AuthorResponse firstAuthor = new AuthorResponse(
+                author.id().uuid().toString(),
+                author.version().toString(),
+                author.name(),
+                Map.of()
+        );
+
+        final AuthorResponse secondAuthor = new AuthorResponse(
+                null,
+                null,
+                "Author, Second",
+                Map.of()
+        );
+
+        final BookResponse bookResponse = new BookResponse(
+                book.id().schema().name(),
+                book.id().id(),
+                book.title(),
+                List.of(firstAuthor, secondAuthor),
+                book.keywords(),
+                List.of(MimeTypes.EPUB.toString())
+        );
+
+        final Author createdAuthor = new Author(AuthorId.withoutId(), secondAuthor.name());
+
+        when(bookInquiryService.authorById(author.id())).thenReturn(Optional.of(author));
+        when(bookUpdateService.registerAuthor(secondAuthor.name(), Map.of())).thenReturn(createdAuthor);
+
+        when(bookUpdateService.registerBook(
+                eq(book.id()),
+                eq(book.title()),
+                eq(List.of(author, createdAuthor)),
+                eq(BookResponse.toDomain(bookResponse.formats())),
+                eq(book.keywords())
+        )).thenReturn(book);
+
+        final BookResponse createdBook = bookController.postBook(bookResponse);
+        assertNotNull(createdBook);
+
+        verify(bookInquiryService, times(1)).authorById(author.id());
+        verify(bookUpdateService, times(1)).registerAuthor(secondAuthor.name(), Map.of());
     }
 }
