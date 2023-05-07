70f9578f10688c06a9428129d23bc2e70f3e702f
Rene van Putten <z669016@gmail.com>
2023-05-07 20:39:45 +0200

    removed unused import

--- src/test/java/com/putoet/mybooks/books/adapter/in/web/security/UserControllerTest.java
+++ src/test/java/com/putoet/mybooks/books/adapter/in/web/security/UserControllerTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class UserControllerTest {
